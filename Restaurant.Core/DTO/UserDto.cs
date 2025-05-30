245cb251961eb99e9ac0941520ccb0f1b329d725
Seif Elmoazen <seifhesham882@gmail.com>
2025-05-30 20:24:59 +0700

    Implemented Profile services and the basket service.

---
+++ Restaurant.Core/DTO/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Core.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public DateTime? BirthDate { get; set; }
+        public int Gender { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
